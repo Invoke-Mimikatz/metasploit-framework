676d375eaf6942be3c4b3195e234e2a9296d2e0e
Kevin Clark <kevin@ts-lappy.local>
2022-08-28 20:08:22 -0500

    converting kiwi to bofloader

--- lib/rex/post/meterpreter/extensions/bofloader/command_ids.rb
+++ lib/rex/post/meterpreter/extensions/bofloader/command_ids.rb
@@ -3,13 +3,13 @@ module Rex
 module Post
 module Meterpreter
 module Extensions
-module Kiwi
+module Bofloader
 
 # ID for the extension (needs to be a multiple of 1000)
-EXTENSION_ID_KIWI = 8000
+EXTENSION_ID_BOFLOADER = 99000
 
 # Associated command ids
-COMMAND_ID_KIWI_EXEC_CMD = EXTENSION_ID_KIWI + 1
+COMMAND_ID_BOFLOADER_EXEC_CMD = EXTENSION_ID_BOFLOADER + 1
 
 end
 end
